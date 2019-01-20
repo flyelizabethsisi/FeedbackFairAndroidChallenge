2eb6ed68363199e6e4148ce56c2b788d0f19e5d5
Victoria Leon <vcrleon@gmail.com>
2019-01-19 20:03:44 -0500

    added TODO comments for xml layouts

--- app/src/main/java/org/pursuit/dogbreeds/DisplayActivity.java
+++ app/src/main/java/org/pursuit/dogbreeds/DisplayActivity.java
@@ -18,6 +18,13 @@ public class DisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+        /* TODO: Create xml layout for DisplayActivity in res folder using layout type of your choice
+           TODO: Name this layout activity_display.xml
+           TODO: Change background color to a color of your choice
+           TODO: Add TextView to display breed & align the view to the center of the screen
+           TODO: Add ImageView to display dog image & align view to the center of the screen */
+
+
         breedTextView = findViewById(R.id.selected_breed_textview);
         beedImageView = findViewById(R.id.random_dog_imageview);
     }
